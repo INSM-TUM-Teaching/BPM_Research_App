abf5f3c6c506a4a1ee74abf01cea3a426c3407da
anishabasnet7 <anishabasnet50@gmail.com>
2025-06-28 00:42:16 +0200

    Remove GUI folder files

--- gui/src/types/bpmn-js.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "bpmn-js/lib/Modeler" {
-  export default class BpmnModeler {
-    constructor(opts?: any);
-    importXML(xml: string): Promise<any>;
-    saveXML(opts?: any): Promise<{ xml: string }>;
-    saveSVG(opts?: any): Promise<{ svg: string }>;
-    destroy(): void;
-  }
-}
